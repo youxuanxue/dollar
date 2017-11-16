f966958f3984f838d5c610deeb1a90e823b80011
Jiao Xue <xuejiao@wandoujia.com>
2017-11-16 10:54:20 +0800

    Remove storyboard and sks

--- dollar/AppDelegate.swift
+++ dollar/AppDelegate.swift
@@ -14,10 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            window.backgroundColor = UIColor.white
+            window.rootViewController = UIViewController()
+            window.makeKeyAndVisible()
+        }
     return true
     }
 
--- dollar/GameViewController.swift
+++ dollar/GameViewController.swift
@@ -14,22 +14,10 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+
+        let scene = GameScene(size: view.frame.size)
+        let skView = view  as! SKView
+        skView.presentScene(scene)
     }
 
     override var shouldAutorotate: Bool {
